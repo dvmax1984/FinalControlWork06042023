2885104c1e61c1681625e3f501034156b12b981b
dvmax1984 <dvmax1984@mail.ru>
2023-04-07 23:45:41 +0700

    рабочий вариант

--- Program.cs
+++ Program.cs
@@ -5,22 +5,23 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []*/
 
-Console.WriteLine("Hello, World!");
-
-string[] array = {"Hello", "2", "world"};
+string[] array = {"Hello", "2", "world", ":-)"};
+string[] result = new string[4];
 
 int n = 0;
 
 for (int m=0; m < array.Length; m++)
 {
-    string[] result;
-    
-    //LastIndexOf: находит индекс последнего вхождения символа или подстроки в строке
-    
     if (array[m].Length <= 3)
     {
-        result[n] = array[m];
-        n = n + 1;
+        result[n] = array[m].ToString();
+        n =+1;
+        //Console.WriteLine(array[m].ToString());
     }
 }
 
+for (int i=0; i < result.Length-1; i++)
+{
+    Console.WriteLine(result[i].ToString());
+
+}
